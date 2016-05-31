c3835ef691a56119e2c20877fa340e0eab1d38c2
Tejas Dinkar <tejas@gja.in>
2016-05-31 12:31:37 +0530

    Gracefully failing if api_host is not set

--- lib/quintype/routes/railtie.rb
+++ lib/quintype/routes/railtie.rb
@@ -3,11 +3,8 @@ module Quintype::Routes
     config.quintype_routes = ActiveSupport::OrderedOptions.new
 
     initializer "quintype-routes.register_routes" do |app|
+      api_host = app.config.quintype_routes.quintype_host
       app.config.middleware.insert_before(Rack::Runtime, Rack::ReverseProxy) do
-        api_host = app.config.quintype_routes.quintype_host
-
-        raise "Please set config.quintype_routes.quintype_host" unless app.config.quintype_routes.quintype_host
-
         reverse_proxy '/api', api_host
         reverse_proxy '/login', api_host
         reverse_proxy '/auth.form', api_host
@@ -18,7 +15,7 @@ module Quintype::Routes
         reverse_proxy '/rss-feed', api_host
         reverse_proxy '/stories.rss', api_host
         reverse_proxy '/news_sitemap.xml', api_host
-      end
+      end if api_host
     end
   end
 end
