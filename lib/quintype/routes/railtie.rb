5cfda951d9fecd6a3beae2cc20bb00b827dc93a5
Tejas Dinkar <tejas@gja.in>
2016-09-06 16:50:23 +0530

    All login related routes

--- lib/quintype/routes/railtie.rb
+++ lib/quintype/routes/railtie.rb
@@ -8,6 +8,8 @@ module Quintype::Routes
         reverse_proxy '/api', api_host
         reverse_proxy '/login', api_host
         reverse_proxy '/auth.form', api_host
+        reverse_proxy '/auth.callback', api_host
+        reverse_proxy '/auth', api_host
         reverse_proxy '/admin', api_host
         reverse_proxy '/shrubbery', api_host
         reverse_proxy '/sitemap', api_host
