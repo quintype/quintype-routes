445b9eaa1c5fb1451865e0319dbb0bb9021ee46a
Tejas Dinkar <tejas@gja.in>
2016-09-22 17:00:03 +0530

    Fixing auth and login

--- lib/quintype/routes/railtie.rb
+++ lib/quintype/routes/railtie.rb
@@ -6,10 +6,10 @@ module Quintype::Routes
       api_host = app.config.quintype_routes.quintype_host
       app.config.middleware.insert_before(Rack::Runtime, Rack::ReverseProxy) do
         reverse_proxy '/api', api_host
-        reverse_proxy '/login', api_host
+        reverse_proxy /^\/login(\?.*)?$/, api_host
         reverse_proxy '/auth.form', api_host
         reverse_proxy '/auth.callback', api_host
-        reverse_proxy /^\/auth$/, api_host
+        reverse_proxy /^\/auth(\?.*)?$/, api_host
         reverse_proxy '/admin', api_host
         reverse_proxy '/shrubbery', api_host
         reverse_proxy '/sitemap', api_host
